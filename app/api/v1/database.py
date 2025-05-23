91c5f144a5151021fdf4ec651c537a5cfa4c5455
Kirfi <kirbel357@gmail.com>
2025-05-24 00:08:12 +0500

    feat: update

--- app/api/v1/database.py
+++ app/api/v1/database.py
@@ -3,7 +3,13 @@ from typing import Dict, List
 
 from app.api.v1.schemas import Instrument, L2OrderBook, Transaction, Order
 
-users: Dict[str, Dict] = {}
+users: Dict[str, Dict] = {
+    "admin": {
+        "username": "admin",
+        "role": "ADMIN",
+        "api_key": "6746ada2-42d8-49ab-bab6-692cfe733dbb"
+    }
+}
 instruments: List[Instrument] = [Instrument(name="ToyCoin", ticker="TOY")]
 balances: Dict[str, Dict[str, float]] = {}
 orderbooks: Dict[str, L2OrderBook] = {}
